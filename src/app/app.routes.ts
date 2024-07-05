30045fa5560e9d386e3b95114a17f8b1f01a064c
FSchmuck <florian.schmuck@bearingpoint.com>
2024-07-05 08:26:40 +0200

    remove unused components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,25 +1,6 @@
-import {Route, Routes} from '@angular/router';
-import {AppConfig} from "./app.config.type";
-import {ConfiguratorComponent} from "./configurator/configurator.component";
+import {Routes} from '@angular/router';
+import {ConfiguratorComponent} from './configurator/configurator.component';
 
-export function createRoutesFromConfig(appConfig: AppConfig): Routes {
-  if (!appConfig) {
-    return [];
-  }
-  const keys = Object.keys(appConfig)
-  return keys.map(key => {
-    const path = `form/${key}`;
-    console.log(path)
-    return ({
-      path: path,
-      loadComponent: () => import('./item-form/item-form.component')
-        .then(c => c.ItemFormComponent),
-      data: {
-        configuration: appConfig[key]
-      }
-    } as Route)
-  })
-}
 
 export const routes: Routes = [{
   path: '',
