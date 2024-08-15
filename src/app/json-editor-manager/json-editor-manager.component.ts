28b524e519dd1c2b51587899c09373c1028688e5
FSchmuck <florian.schmuck@bearingpoint.com>
2024-08-15 10:27:49 +0200

    Introduce jsonrepair to fix invalid json input automatically

--- src/app/json-editor-manager/json-editor-manager.component.ts
+++ src/app/json-editor-manager/json-editor-manager.component.ts
@@ -5,6 +5,7 @@ import {MatDrawer, MatDrawerContainer, MatDrawerContent} from '@angular/material
 import {BehaviorSubject} from 'rxjs';
 import {AsyncPipe, JsonPipe} from '@angular/common';
 import {MatButton} from '@angular/material/button';
+import {jsonrepair} from 'jsonrepair';
 
 @Component({
   selector: 'app-json-editor-manager',
@@ -27,15 +28,22 @@ export class JsonEditorManagerComponent {
   fileContent: any | undefined = undefined;
   showPreview = false;
   // @ts-ignore
-  private previewSubject:BehaviorSubject<string | undefined> = new BehaviorSubject(undefined)
+  private previewSubject: BehaviorSubject<string | undefined> = new BehaviorSubject(undefined);
   preview = this.previewSubject.asObservable();
+
   whenFileContentLoaded(fileContent: string) {
     if (fileContent) {
       try {
         this.fileContent = JSON.parse(fileContent);
-      this.previewSubject.next(this.fileContent)
+        this.previewSubject.next(this.fileContent);
       } catch {
-        throw new Error('Invalid JSON in file content');
+        console.error('Invalid JSON..repairing...');
+        try {
+          const repairedJson = jsonrepair(fileContent);
+          this.fileContent = JSON.parse(repairedJson);
+        } catch {
+          throw new Error('Invalid JSON in file content');
+        }
       }
     }
   }
@@ -45,13 +53,13 @@ export class JsonEditorManagerComponent {
   }
 
   whenDataChanged(data: string) {
-    this.previewSubject.next(data)
+    this.previewSubject.next(data);
   }
 
   downloadJson() {
     const jsonString = JSON.stringify(this.previewSubject.getValue());
-    const blob = new Blob([jsonString], { type: 'application/json' });
-    const file = new File([blob], 'data.json', { type: 'application/json' });
+    const blob = new Blob([jsonString], {type: 'application/json'});
+    const file = new File([blob], 'data.json', {type: 'application/json'});
 
     const url = window.URL.createObjectURL(file);
     const a = document.createElement('a');
